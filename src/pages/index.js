efe46c97e70208c7ecfd12ad77d701bd7b706206
Peter Cruckshank <petecruckshank@gmail.com>
2020-05-01 21:08:09 -0400

    swapped out Image for MainSVG

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,16 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import { Link } from 'gatsby'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import MainSVG from '../components/MainSVG'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <p>Welcome to your new Gatsby site. 🚀 </p>
+
+    <MainSVG />
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
